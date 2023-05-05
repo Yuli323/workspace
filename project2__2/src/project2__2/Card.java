d3e109d99d31d4108741b9a5aa1b0fa7ad0582be
Yuli323 <a0979505485@gmail.com>
2023-05-05 09:16:15 +0800

    first-time

---
+++ project2__2/src/project2__2/Card.java
@@ -0,0 +1,7 @@
+package project2__2;
+
+public class Card {
+    String face;
+    String suit;
+    
+}
---
+++ project2__2/src/project2__2/DeckOfCard.java
@@ -0,0 +1,64 @@
+package project2__2;
+
+import java.security.SecureRandom;
+
+public class DeckOfCard {
+        String[] faces = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        String[] suits = { "♧", "♢", "♡", "♤" };
+        Card[] deckOfCard = new Card[52];
+        int count;
+        
+    public DeckOfCard() {
+        for(int i = 0; i < deckOfCard.length; i++) {
+            deckOfCard[i] = new Card();
+            deckOfCard[i].face= faces [i %13];
+            deckOfCard[i].suit=  suits[i/13];
+        }
+    }
+        
+    public void shuffle(boolean isused) {
+        SecureRandom randomNum = new SecureRandom();
+        int i;
+        int randNum;
+        int times;
+        if(!isused) {
+            i = count;
+            randNum = randomNum.nextInt(52 - count) + count;
+            times = deckOfCard.length;
+        }else {
+            i = 0;
+            randNum = randomNum.nextInt(count);
+            times = count;
+        
+    }
+           
+        
+        
+        for(; i < times; i++) {
+        Card tempFace = deckOfCard[randNum];
+        deckOfCard[randNum]= deckOfCard[i]; 
+        deckOfCard[i]= tempFace;
+       
+        }
+    }
+    
+    public void dealCard(int randNum,boolean isused) {
+        int LocalCount = count;
+        LocalCount = 0;
+        
+        if(isused)
+            count = 0;
+        
+        for(int i = 0; i < randNum; i++) {
+            Card card = deckOfCard[LocalCount];
+            LocalCount++;
+            System.out.println(card.face + "_" + card.suit);
+        }
+           if(isused)
+           count = LocalCount;
+    }
+   
+        
+}
+    
+
